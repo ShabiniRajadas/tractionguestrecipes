cedb1cd3a273bd803aa5072eccc7a246e39da0bf
ShabiniRajadas <shabiniraja90@gmail.com>
2019-07-27 23:55:00 +0400

    Defined database credentials

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,5 @@ module RecipeManagementApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.debug_exception_response_format is set to :api
   end
 end
