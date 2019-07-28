0a4e4c35a91f4c725ee101b4dc1473fe8fac7cd9
ShabiniRajadas <shabiniraja90@gmail.com>
2019-07-29 00:40:43 +0400

    rubocop fixes done

--- app/controllers/backoffice/v1/companies_controller.rb
+++ app/controllers/backoffice/v1/companies_controller.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
 module Backoffice
   module V1
     class CompaniesController < ::Backoffice::ApplicationController
     end
-end
+  end
 end
--- spec/factories/company.rb
+++ spec/factories/company.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
-  # The model spec file company_spec.rb get loaded before the factories and throws duplicate error
+  # The model spec file company_spec.rb gets loaded
+  # before the factories and throws duplicate error
   # So company renamed as :company_record
   factory :company_record, class: Company do
     sequence(:uid) { |n| "#{n}-company" }
