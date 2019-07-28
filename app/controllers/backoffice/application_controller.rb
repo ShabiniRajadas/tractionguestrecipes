5fc50ccb85a5ffd1df0aaa56300497aeb9c6a3e1
ShabiniRajadas <shabiniraja90@gmail.com>
2019-07-29 01:02:37 +0400

    Added HTTP basic authentication for backoffice APIs

--- app/controllers/backoffice/application_controller.rb
+++ app/controllers/backoffice/application_controller.rb
@@ -1,4 +1,17 @@
-# frozen_string_literal: true
+module Backoffice
+  class ApplicationController < ActionController::API
+    include ActionController::HttpAuthentication::Basic::ControllerMethods
 
-class ApplicationController < ActionController::API
+    before_action :htauth
+
+    respond_to :json
+
+    private
+
+    def htauth(name = ENV["BACKOFFICE_HTAUTH_USERNAME"], password = ENV["BACKOFFICE_HTAUTH_PASSWORD"])
+      authenticate_or_request_with_http_basic do |user_name, user_password|
+        (user_name == name && user_password == password)
+      end
+    end
+  end
 end
