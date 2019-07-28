b08daaa8e25d8bcfdf19613df9b64347265acadc
ShabiniRajadas <shabiniraja90@gmail.com>
2019-07-29 01:40:40 +0400

    Added controller structure for companies controller APIs

--- app/controllers/backoffice/v1/companies_controller.rb
+++ app/controllers/backoffice/v1/companies_controller.rb
@@ -1,6 +1,26 @@
 module Backoffice
   module V1
     class CompaniesController < ::Backoffice::ApplicationController
+    	def index
+    	end
+
+    	def create
+    	end
+
+    	def show
+    	end
+
+    	def update
+    	end
+
+    	def destroy
+    	end
+
+    	private
+
+    	def permitted_params
+    		params.require(:data).require(:attributes).require(:name, :description, :url, :image)
+    	end
     end
   end
 end
