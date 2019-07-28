9c368f692ca10c527708dcd07c522893bbd991be
ShabiniRajadas <shabiniraja90@gmail.com>
2019-07-29 00:17:31 +0400

    rubocop fixes for schema and gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_28_191230) do
-
-  create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "uid", null: false
-    t.string "name"
-    t.string "url"
-    t.string "description"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema.define(version: 20_190_728_191_230) do
+  create_table 'companies', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.string 'uid', null: false
+    t.string 'name'
+    t.string 'url'
+    t.string 'description'
+    t.string 'image'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
