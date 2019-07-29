1d742704c9a04942d0be5220188c8eadbe075665
ShabiniRajadas <shabiniraja90@gmail.com>
2019-07-29 10:40:59 +0400

    added error serializer

---
+++ app/serializers/backoffice/v1/error_serializer.rb
@@ -0,0 +1,19 @@
+module Backoffice
+  module V1
+    module ErrorSerializer
+      def self.serialize(errors)
+        return if errors.nil?
+
+        result = {}
+        new_hash = errors.to_hash(true).map do |key, value|
+          value.map do |msg|
+            {title: key, code: key, details: msg}
+          end
+        end
+        new_hash = new_hash.flatten
+        result[:errors] = new_hash
+        result
+      end
+    end
+  end
+end
