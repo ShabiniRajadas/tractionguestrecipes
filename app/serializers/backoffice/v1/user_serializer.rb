4428a787e27071ec9c36e29792dbe722e6a2e432
ShabiniRajadas <shabiniraja90@gmail.com>
2019-07-31 00:19:55 +0400

    added rspecs for create method in users controller

--- app/serializers/backoffice/v1/user_serializer.rb
+++ app/serializers/backoffice/v1/user_serializer.rb
@@ -2,7 +2,7 @@ module Backoffice
   module V1
     class UserSerializer < ActiveModel::Serializer
       type 'user'
-      attributes :id, :name, :email
+      attributes :name, :email
     end
   end
 end
--- spec/controllers/backoffice/v1/users_controller_spec.rb
+++ spec/controllers/backoffice/v1/users_controller_spec.rb
@@ -18,4 +18,41 @@ RSpec.describe Backoffice::V1::UsersController do
       expect(response).to be_successful
     end
   end
+
+  describe '#create' do
+    let(:user_params) do
+      {
+        data: {
+          type: 'user',
+          attributes: {
+            email: 'sample@sample.com'
+          }
+        }
+      }
+    end
+    let(:do_request) { post(:create, params: user_params) }
+
+    before do
+      http_login
+    end
+
+    it 'responds with success' do
+      do_request
+      expect(response).to be_successful
+    end
+
+    it 'responds with companies' do
+      do_request
+      expect(json_response_body).to eq(
+        'data' => {
+          'attributes' => {
+            'email' => 'sample@sample.com',
+            'name' => ''
+          },
+          'id' => json_response_body['data']['id'],
+          'type' => 'user'
+        }
+      )
+    end
+  end
 end
