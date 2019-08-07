4fdc81cb5c0baa48eec854085077558c05dd7044
ShabiniRajadas <shabiniraja90@gmail.com>
2019-08-07 21:56:12 +0400

    added action mailer configuration in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.active_job.queue_adapter = :sidekiq
+  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: ENV['ADDRESS'],
+      port: ENV['PORT'],
+      domain: ENV['DOMAIN'],
+      user_name: ENV['USER_NAME'],
+      password: ENV['PASSWORD'],
+      authentication: 'login',
+      enable_starttls_auto: false
+    }
 end
