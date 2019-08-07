445951d29652276027e2c3dc89b039882f1e22c7
ShabiniRajadas <shabiniraja90@gmail.com>
2019-08-07 21:50:52 +0400

    use sidekiq for delivering messages asynchronously

--- app/jobs/user_job.rb
+++ app/jobs/user_job.rb
@@ -2,10 +2,7 @@ class UserJob < ApplicationJob
   queue_as :default
 
   def perform(user_id)
-  	p "USER ID DETAILS"
-  	p user_id
     user = User.find_by(id: user_id)
-    p user
-    UserMailer.welcome_email(user).deliver_now
+    UserMailer.welcome_email(user).deliver_later
   end
 end
